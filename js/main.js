1bf84e1bf958a8589db5b7b0ee45ed58933b4799
EvgenyJj <91139752+EvgenyJj@users.noreply.github.com>
2024-03-11 19:26:48 +0300

    Добавление вспомогательных функций

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-const getRandomIntegerFromRange = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const getRandomIntegerFromRange = function (min, max) {
+  if (min >= max) {
+    throw new Error('Не верно указан диапазон: значение "от" первышет или равно "до"');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const checkingMaximumStringLength = (string, length) => length >= string.length;
 
+getRandomIntegerFromRange(15, 55);
