6b4deeeac95a5c4362798a6feb790a0492511fbe
EvgenyJj <91139752+EvgenyJj@users.noreply.github.com>
2024-04-10 18:22:16 +0300

    Добавление фильтрации постов

--- js/util.js
+++ js/util.js
@@ -13,4 +13,20 @@ const isEscapeKey = (evt) => {
   return evt.key === 'Escape';
 };
 
-export {getRandomIntegerFromRange, getRandomArrayElement, isEscapeKey};
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomIntegerFromRange, getRandomArrayElement, isEscapeKey, shuffleArray, debounce};
